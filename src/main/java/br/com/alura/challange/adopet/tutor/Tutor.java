f8751402d2cb2483593baa32184005989850ae47
Paulo1985-git <paulomattos.vv@gmail.com>
2023-04-29 20:08:33 -0600

    [master] - fix: ajusting get method

--- src/main/java/br/com/alura/challange/adopet/tutor/Tutor.java
+++ src/main/java/br/com/alura/challange/adopet/tutor/Tutor.java
@@ -6,11 +6,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Getter
 @EqualsAndHashCode(of = "Id")
 @ToString(of = { "id", "nome", "email" })
 @Entity
@@ -20,19 +18,35 @@ public class Tutor {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private String id;
 	private String nome;
 	private String email;
 	private String senha;
-	
+
 	public Tutor() {
-		
+
 	}
 
 	public Tutor(DadosCadastroTutor dados) {
 		this.nome = dados.nome();
 		this.email = dados.email();
 		this.senha = dados.senha();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getSenha() {
+		return senha;
 	}
 
 }
